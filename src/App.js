2990629ad94c660b036554a8938cd7c70a4872fd
Akshat <kaushal.m@samcomtechnobrains.com>
2022-07-06 12:23:41 +0530

    Backend Connected

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import MoviesList from "./components/MoviesList";
 
 import "./App.css";
+import AddMovie from "./components/AddMovie";
 
 function App() {
   const [movies, setMovies] = useState([]);
@@ -31,31 +32,55 @@ function App() {
 
   //using Async Await
 
-  async function fetchMoviesHandler() {
+  const fetchMoviesHandler = useCallback(async () => {
     console.log("Movies is Fetching");
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch("https://swapi.dev/api/films/");
+      const response = await fetch(
+        "https://backend-project-4b48b-default-rtdb.firebaseio.com/movies.json"
+      );
+      console.log(response);
       if (!response.ok) {
         throw new Error("Something Went Wrong !!!");
       }
       const data = await response.json();
 
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
-      setMovies(transformedMovies);
+      const loadedMovies = [];
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+
+      setMovies(loadedMovies);
       console.log("Movies Fetched!!");
     } catch (error) {
       setError(error.message);
     }
     setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, []);
+
+  async function addMovieHandler(movie) {
+    const response = await fetch(
+      "https://backend-project-4b48b-default-rtdb.firebaseio.com/movies.json",
+      {
+        method: "POST",
+        body: JSON.stringify(movie),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    console.log(data);
   }
 
   let content = <p>Found no movies!</p>;
@@ -72,10 +97,13 @@ function App() {
   return (
     <React.Fragment>
       <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
+      <section>
         <h2>Movies List</h2>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
-      <section>{ content}</section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
